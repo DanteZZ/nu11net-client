f0bb2cfeb12da148b363da110aa37e46e92fd3b4
DanteZZ <dantezzstudio@gmail.com>
2021-01-19 00:50:57 +0700

    Change BIOS

--- assets/scripts/js/index.js
+++ assets/scripts/js/index.js
@@ -1,24 +1,52 @@
 const _vm = require("vm");
 const _fs = require("fs");
 const _util = require("util");
-const _sBinds = Seemple.binders;
-
 
 
 // Create Seemple objects
-class _smpBIOS extends Seemple {
-    constructor() {
-        super();
-        this.loadChars = ["-","\\","|","/"];
-        this.loadId = 0;
-        this.title = "Инициализация";
-        setInterval(function(){
-        	BIOS.title = "Загрузка системы "+BIOS.loadChars[BIOS.loadId];
-        	BIOS.loadId++;
-        	if (BIOS.loadId == BIOS.loadChars.length) {BIOS.loadId = 0;};
-        },200);
-        this.bindNode('title', 'tlt', _sBinds.html());
-    }
-}
-
-const BIOS = new _smpBIOS();
+var BIOS = new Vue({
+  el: '#__bios',
+  data: {
+    title: "HIOS - Настройка CMOS",
+	footer: "v0.1 (C) Copyright 2015-2021, H4CK-IT, Inc",
+	selItem: 0,
+	selSection: 0,
+	menuList: [
+		{
+			title: "Основное",
+			defaultSections:"auth",
+			sections:[
+				{
+					title:"Авторизация",
+					action:function(){
+						alert("Авторизация");
+					}
+				}
+			]
+		},
+		{
+			title: "Настройки",
+			defaultSections:"auth",
+			sections:[
+				{
+					title:"Авторизация",
+					action:function(){
+						alert("Авторизация");
+					}
+				}
+			]
+		}
+	]
+  },
+  mounted: function() {
+  	// Проверка нажати клавиш
+  	this.onkeyup = window.addEventListener('keyup', function(e) {
+  		let b = BIOS;
+  		if (e.which == "39") {//Right arrow
+  			if (b.selItem+1 == b.menuList.length) { b.selItem = 0;} else {b.selItem++;};
+  		} else if (e.which == "37") {//Right arrow
+  			if (b.selItem-1 == -1) { b.selItem = b.menuList.length-1;} else {b.selItem--;};
+  		};
+  	});
+  }
+});
