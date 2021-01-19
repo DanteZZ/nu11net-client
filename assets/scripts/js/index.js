05a4e15eb00c526592f23d080868ff3e71cae0fc
DanteZZ <dantezzstudio@gmail.com>
2021-01-19 16:57:51 +0700

    Update BIOS

--- assets/scripts/js/index.js
+++ assets/scripts/js/index.js
@@ -29,7 +29,19 @@ var BIOS = new Vue({
 			defaultSections:"auth",
 			sections:[
 				{
-					title:"Авторизация",
+					title:"Авторизация 2",
+					action:function(){
+						console.log(this);
+					}
+				},
+				{
+					title:"Авторизация 2",
+					action:function(){
+						alert("Авторизация");
+					}
+				},
+				{
+					title:"Авторизация 2",
 					action:function(){
 						alert("Авторизация");
 					}
@@ -38,15 +50,33 @@ var BIOS = new Vue({
 		}
 	]
   },
+
+  methods: {
+  	enterSection: function(section) {
+  		section.action();
+  	}
+  },
+
   mounted: function() {
   	// Проверка нажати клавиш
   	this.onkeyup = window.addEventListener('keyup', function(e) {
   		let b = BIOS;
-  		if (e.which == "39") {//Right arrow
+  		if (e.which == 39) {//Right arrow
   			if (b.selItem+1 == b.menuList.length) { b.selItem = 0;} else {b.selItem++;};
-  		} else if (e.which == "37") {//Right arrow
+  			b.selSection = 0;
+  		} else if (e.which == 37) {//Left arrow
   			if (b.selItem-1 == -1) { b.selItem = b.menuList.length-1;} else {b.selItem--;};
+  			b.selSection = 0;
   		};
+  		if (e.which == 40) {//Down arrow
+  			if (b.selSection+1 == b.menuList[b.selItem].sections.length) { b.selSection = 0;} else {b.selSection++;};
+  		} else if (e.which == 38) {//Up arrow
+  			if (b.selSection-1 == -1) { b.selSection = b.menuList[b.selItem].sections.length-1;} else {b.selSection--;};
+  		};
+
+  		if (e.which == 13) {//Enter
+  			b.enterSection(b.menuList[b.selItem].sections[b.selSection]);
+  		}
   	});
   }
 });
