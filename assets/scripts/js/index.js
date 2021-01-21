e4f10e8b4d4131d879a660319459dddeffa93504
DanteZZ <dantezzstudio@gmail.com>
2021-01-21 17:03:27 +0700

    Bios Modals Update

--- assets/scripts/js/index.js
+++ assets/scripts/js/index.js
@@ -14,7 +14,9 @@ var BIOS = new Vue({
 		selSection: 0,
 		modal: {
 			opened:false,
-			title:""
+			title:"",
+			selItem:0,
+			selItems:[]
 		},
 
 		serverList: {
@@ -22,10 +24,15 @@ var BIOS = new Vue({
 				name:"Локальный сервер",
 				ip: "127.0.0.1",
 				port:"1337"
+			},
+			"localhost:13372":{
+				name:"Локальный сервер",
+				ip: "127.0.0.1",
+				port:"13372"
 			}
 		},
 		userData: {
-			selectedServer: "localhost",
+			selectedServer: "localhost:1337",
 			auth: {
 				login: "",
 				password: ""
@@ -44,7 +51,7 @@ var BIOS = new Vue({
 							type:"select",
 							values:"serverList",
 							optionTitle:["name"," [","ip",":","port","]"],
-							model:"selectedServer"
+							model:"userData.selectedServer"
 						}
 					}
 				]
@@ -89,10 +96,29 @@ var BIOS = new Vue({
 	  		};
 	  	},
 	  	getVar: function(name){
-	  		return this[name];
+	  		let vars = name.split('.');
+	  		let res = this;
+	  		for (var k in vars) {
+	  			res = res[vars[k]];
+	  		};
+	  		return res;
 	  	},
 	  	closeModal: function() {
 	  		this.modal.opened = false;
+	  	},
+	  	enterModal: function() {
+	  		switch (this.modal.type) {
+	  			case "select":
+	  				let model = this;
+	  				let vars = this.modal.model.split('.');
+			  		for (var k in vars) {
+			  			if (k == vars.length-1) { break; };
+			  			model = model[vars[k]];
+			  		};
+			  		model[vars.pop()] = this.modal.selItems[this.modal.selItem];
+			  		this.closeModal();
+	  			break;
+	  		};
 	  	},
 	  	genTitle: function(arr,vals=false){
 	  		if (typeof(arr) == "object") {
@@ -114,29 +140,54 @@ var BIOS = new Vue({
 	  		for (var k in modal) {
 	  			this.modal[k] = modal[k];
 	  		};
+	  		switch (modal.type) {
+	  			case "select":
+	  				this.modal.selItems = [];
+	  				for (var k in this.getVar(this.modal.values)) {
+	  					this.modal.selItems.push(k);
+	  				};
+	  				this.modal.selItem = this.modal.selItems.indexOf(this.getVar(this.modal.model));
+	  			break;
+	  		};
 	  		this.modal.opened = true;
 	  	},
 	  	onkeyup: function(e) {
 	  		let b = BIOS;
 	  		if (e.which == 39) {//Right arrow
-	  			if (b.selItem+1 == b.menuList.length) { b.selItem = 0;} else {b.selItem++;};
-	  			b.selSection = 0;
+	  			if (!b.modal.opened) {
+	  				if (b.selItem+1 == b.menuList.length) { b.selItem = 0;} else {b.selItem++;};
+	  				b.selSection = 0;
+	  			};
 	  		} else if (e.which == 37) {//Left arrow
-	  			if (b.selItem-1 == -1) { b.selItem = b.menuList.length-1;} else {b.selItem--;};
-	  			b.selSection = 0;
+	  			if (!b.modal.opened) {
+		  			if (b.selItem-1 == -1) { b.selItem = b.menuList.length-1;} else {b.selItem--;};
+		  			b.selSection = 0;
+		  		};
 	  		};
 	  		if (e.which == 40) {//Down arrow
-	  			if (b.selSection+1 == b.menuList[b.selItem].sections.length) { b.selSection = 0;} else {b.selSection++;};
+	  			if (!b.modal.opened) {
+	  				if (b.selSection+1 == b.menuList[b.selItem].sections.length) { b.selSection = 0;} else {b.selSection++;};
+	  			} else {
+	  				if (b.modal.selItem+1 == b.modal.selItems.length) { b.modal.selItem = 0;} else {b.modal.selItem++;};
+	  			};
 	  		} else if (e.which == 38) {//Up arrow
-	  			if (b.selSection-1 == -1) { b.selSection = b.menuList[b.selItem].sections.length-1;} else {b.selSection--;};
+	  			if (!b.modal.opened) {
+	  				if (b.selSection-1 == -1) { b.selSection = b.menuList[b.selItem].sections.length-1;} else {b.selSection--;};
+	  			} else {
+	  				if (b.modal.selItem-1 == -1) { b.modal.selItem = b.modal.selItems.length-1;} else {b.modal.selItem--;};
+	  			};
 	  		};
 
 	  		if (e.which == 13) {//Enter
-	  			b.enterSection(b.menuList[b.selItem].sections[b.selSection]);
+	  			if (!b.modal.opened) {
+	  				b.enterSection(b.menuList[b.selItem].sections[b.selSection]);
+	  			} else {
+	  				b.enterModal();
+	  			};
 	  		};
 
 	  		if (e.which == 27) {//Escape
-	  			if (b.isModal) {b.closeModal();};
+	  			if (b.modal.opened) {b.closeModal();};
 	  		}
 	  	}
   	},
